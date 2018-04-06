8494a70c555d9e89bc3df3db3bfc17d7c84b8ee0
lausek <root@lausek.eu>
2018-04-06 17:14:13 +0200

    compress css too

--- compress.sh
+++ compress.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-SERVICE_URL="https://javascript-minifier.com/raw"
+JS_SERVICE_URL="https://javascript-minifier.com/raw"
+CSS_SERVICE_URL="https://cssminifier.com/raw"
 
-curl -X POST --data-urlencode "input@lgal.js" $SERVICE_URL > lgal.min.js
+curl -X POST --data-urlencode "input@lgal.js" $JS_SERVICE_URL > lgal.min.js
+
+curl -X POST --data-urlencode "input@lgal.css" $CSS_SERVICE_URL > lgal.min.css
